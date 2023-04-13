93d0cebf2dc63ddb394cd4245684483709ab7c4e
arjunk7 <arjunkkirde@gmail.com>
2023-04-13 11:36:50 +0530

    mysql connection established

---
+++ config.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'rOOT#1885',
+    database : 'TIF'
+})
+
+conn.connect((error)=>{
+    if(error){
+        console.log('error connecting to mysql database'+ error.stack);
+        return;
+    }
+    console.log('Connected successfully with thread ID'+ conn.threadId);
+})
+
+module.exports = conn;
--- routes.js
+++ routes.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('./config');
+
+//bodyparser to get the details given in form
+var bodyparser = require('body-parser');
+
+//convert data to json format
+router.use(bodyparser.json);
+router.use(bodyparser.urlencoded({extended:true}));
 
 /*                            intex route                                              */
 
@@ -10,13 +18,31 @@ router.get('/',(req,res)=>{
 /*                            role routes                                              */
 
 router.get('/v1/role',(req,res)=>{
-    res.send("you are on /v1/role");
+    res.render('createrole');
 })
 
 router.post('/v1/role',(req,res)=>{
-    res.send("you are on /v1/role");
-})
+    var id = req.body.id;
+    var name = req.body.name;
+    //var created_at = Date.now();
+    const date = new Date();
+    const options = { timeZone: 'Asia/Kolkata' };
+    const dateString = date.toLocaleDateString('en-US', options); // Returns something like "4/22/2021"
+    const timeString = date.toLocaleTimeString('en-US', options); // Returns something like "8:47:30 AM"
 
+    const created_at = dateString + ' ' + timeString;
+    const updated_at = created_at;
+
+    conn.connect((error)=>{
+        if(error) throw error;
+        const values =[id, name, created_at,updated_at];
+        var sql = 'INSERT INTO role (id,name,created_at,updated_at) VALUES ?';
+        conn.query(sql,[values],(error,result)=>{
+            if(err) throw err;
+            res.send("role created successfull"+result.insertId);
+        })
+    })
+})
 
 /*                            user routes                                              */
 
@@ -26,7 +52,28 @@ router.get('/v1/auth/signup', (req, res) => {
 })
 
 router.post('/v1/auth/signup', (req, res) => {
-    res.render('signup')
+    var id = req.body.id;
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    //var created_at = Date.now();
+    const date = new Date();
+    const options = { timeZone: 'Asia/Kolkata' };
+    const dateString = date.toLocaleDateString('en-US', options); // Returns something like "4/22/2021"
+    const timeString = date.toLocaleTimeString('en-US', options); // Returns something like "8:47:30 AM"
+
+    const created_at = dateString + ' ' + timeString;
+
+
+    conn.connect((error)=>{
+        if(error) throw error;
+        const values =[id, name, email, password,created_at];
+        var sql = 'INSERT INTO user (id,name,email,password,created_at) VALUES ?';
+        conn.query(sql,[values],(error,result)=>{
+            if(err) throw err;
+            res.send("role created successfull"+result.insertId);
+        })
+    })
 })
 
 router.get('/v1/auth/signin', (req, res) => {
